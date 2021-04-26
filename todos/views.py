c16b78fc37d632671d92b7289ecc0cdc3d5ee8c7
Charles Benin <cbenin88@gmail.com>
2021-04-26 13:43:24 -0700

    Initial commit

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_page(request):
+    context={}
+    template = 'webpages/home.html'
+    return render(request,template,context)
